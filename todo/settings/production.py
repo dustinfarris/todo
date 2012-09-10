993240eec6868b45046d6fa8b3c02e3f17523330
Dustin Farris <dustin@dustinfarris.com>
2012-09-10 18:12:57 -0400

    Removed Amazon CDN settings

--- todo/settings/production.py
+++ todo/settings/production.py
@@ -2,5 +2,5 @@ from core import *
 
 
 # Amazon CloudFront CDN
-MEDIA_URL = 'http://cdn.todo.com/media/'
-STATIC_URL = 'http://cdn.todo.com/static/'
+#MEDIA_URL = 'http://cdn.todo.com/media/'
+#STATIC_URL = 'http://cdn.todo.com/static/'
